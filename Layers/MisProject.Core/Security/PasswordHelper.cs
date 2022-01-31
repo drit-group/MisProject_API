fad81a9132db249c26993ed3e61604c875c0847f
Taha Ghadirian <taha.ghadirian@outlook.com>
2022-01-31 15:53:12 +0330

    Add project files.

---
+++ Layers/MisProject.Core/Security/PasswordHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MisProject.Core.Security;
+
+public static class PasswordHelper
+{
+    /// <summary>
+    /// Hash password using MD5
+    /// </summary>
+    /// <param name="password">Password you want to hash</param>
+    /// <exception cref="ArgumentException">Password null exception</exception>
+    /// <returns>Hashed password with a salt</returns>
+    public static string HashPasswordMd5(string password)
+    {
+        if (string.IsNullOrEmpty(password))
+            throw new ArgumentException("Password can not be empty", nameof(password));
+
+        var provider = MD5.Create();
+#if DEBUG
+        var salt = "MySalt";
+#else
+        var salt = Environment.GetEnvironmentVariable("MD5_SALT");
+        if (string.IsNullOrEmpty(salt))
+            throw new ArgumentException("MD5_SALT Environment Variable is null!", nameof(salt));
+#endif
+
+        var passwordBytes = Encoding.ASCII.GetBytes(salt + password);
+        var hashedBytes = provider.ComputeHash(passwordBytes);
+        provider.Dispose();
+
+        var sb = new StringBuilder();
+        foreach (var byt in hashedBytes)
+        {
+            sb.Append(byt.ToString("X2"));
+        }
+        return sb.ToString();
+    }
+}
---
+++ Tests/MisProject.Core.Tests/Security/PasswordHelperTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace MisProject.Core.Tests.Security;
+
+public class PasswordHelperTests
+{
+
+#if !DEBUG
+    [SetUp]
+    public void SetUpRelease()
+    {
+        Environment.SetEnvironmentVariable("MD5_SALT", "ReleaseSalt");
+    }
+
+    [TearDown]
+    public void TearDownRelease()
+    {
+        Environment.SetEnvironmentVariable("MD5_SALT", null);
+    }
+
+    [Test]
+    public void HashPassword_EmptySalt_ThrowArgumentException()
+    {
+        // Arrange
+        var password = "password";
+        Environment.SetEnvironmentVariable("MD5_SALT", null);
+
+        // Act
+        Action ex = () => PasswordHelper.HashPasswordMd5(password);
+
+        // Assert
+        ex.Should().Throw<ArgumentException>().WithParameterName("salt");
+    }
+#endif
+
+    [Test]
+    public void HashPasswordMd5_Ok()
+    {
+        // Arrange
+        var password = "password";
+
+#if DEBUG
+        var expected = "471e5243d0db300de1019fed26d3ffb1".ToUpper();
+#else
+        var expected = "d1fb8804db195aa2752f9d4bebc4c6ea".ToUpper();
+#endif
+
+        // Act
+        var actual = PasswordHelper.HashPasswordMd5(password);
+            
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void HashPasswordMd5_EmptyPassword_ThrowArgumentException()
+    {
+        // Arrange
+        string password = "";
+
+        // Act
+        Action ex = () => PasswordHelper.HashPasswordMd5(password);
+
+        // Assert
+        var msg = ex.Should().Throw<ArgumentException>().WithParameterName(nameof(password));
+    }
+}
