dc810e71ca1c317c605d0a8e96440e95dfc5d58e
Taha Ghadirian <taha.ghadirian@outlook.com>
2022-02-01 12:14:30 +0330

    Add DbSettings to main project.

--- MisProject_API/Program.cs
+++ MisProject_API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MisProject.DataLayer.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MisDbContext>(p =>
+{
+    p.UseMySql(connectionString: builder.Configuration.GetConnectionString("MisDbContext"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MisDbContext")));
+});
 
 var app = builder.Build();
 
