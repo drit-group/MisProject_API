c6d334ec44f2070f3c67efbbd2c70e43c41cf99e
Taha Ghadirian <taha.ghadirian@outlook.com>
2022-02-01 17:55:26 +0330

    Add CrudRepository

--- Layers/MisProject.DataLayer/GlobalUsings.cs
+++ Layers/MisProject.DataLayer/GlobalUsings.cs
@@ -1,5 +1,8 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 
+global using MisProject.DataLayer.Repositories.Interfaces;
+global using MisProject.DataLayer.Contexts;
+
 global using MisProject.DTOs.Entities;
 global using MisProject.DTOs.Entities.Users;
 global using MisProject.DTOs.Entities.Permissions;
---
+++ Layers/MisProject.DataLayer/Repositories/Interfaces/ICrudGenericAsync.cs
@@ -0,0 +1,14 @@
+﻿namespace MisProject.DataLayer.Repositories.Interfaces;
+
+public interface ICrudGenericAsync<T>
+{
+    Task<T?> Create(T obj);
+
+    Task<bool> Delete(T obj);
+
+    Task<T?> Update(T obj);
+
+    Task<IEnumerable<T?>> GetAll();
+
+    Task<T?> GetById(int id);
+}
