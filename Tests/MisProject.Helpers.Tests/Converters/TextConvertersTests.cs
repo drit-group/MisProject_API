1b5e7912b688151bb8d2ed3dd38ca883a4d31d52
Taha Ghadirian <taha.ghadirian@outlook.com>
2022-02-01 10:48:21 +0330

    Add Helpers tests.

---
+++ Tests/MisProject.Helpers.Tests/Converters/TextConvertersTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Mime;
+namespace MisProject.Helpers.Tests.Converters;
+
+public class TextConvertersTests
+{
+    [Test]
+    public void ToFixedText_WhenTextIsEmpty_ReturnsEmptyString()
+    {
+        // Arrange
+        var text = string.Empty;
+
+        // Act
+        var result = text.ToFixedText();
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Test]
+    public void ToFixedText_WhenTextIsWhitespace_ReturnsEmptyString()
+    {
+        // Arrange
+        var text = " ";
+
+        // Act
+        var result = text.ToFixedText();
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [TestCase("text", "TEXT")]
+    [TestCase("Text", "TEXT")]
+    [TestCase("Text ", "TEXT")]
+    [TestCase(" Text", "TEXT")]
+    [TestCase(" Text  ", "TEXT")]
+    [TestCase("test.txt", "TEST.TXT")]
+    [TestCase(" test.test@example.com  ", "TEST.TEST@EXAMPLE.COM")]
+    [TestCase("\"@!#test\"<>", "\"@!#TEST\"<>")]
+    public void ToFixedText_WhenTextIsNotNullNorEmptyNorWhitespace_ReturnsFixedText(string text, string excpected)
+    {
+        // Act
+        var result = text.ToFixedText();
+
+        // Assert
+        result.Should().Be(excpected);
+    }
+}
---
+++ Tests/MisProject.Helpers.Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using NUnit.Compatibility;
+global using NUnit.Framework;
+
+global using FluentAssertions;
+
+global using MisProject.Helpers;
+global using MisProject.Helpers.Converters;
