fe697f9611c162a2dd86cb1046e2bf4be828b4fa
Taha Ghadirian <taha.ghadirian@outlook.com>
2022-02-22 19:07:17 +0330

    Add UserRepositoryTests

---
+++ Tests/MisProject.DataLayer.Tests/FixtureConfigs.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+
+namespace MisProject.DataLayer.Tests;
+
+public class UserFixture : Fixture
+{
+    public UserFixture()
+    {
+        this.Register(() => new User()
+        {
+            UserName = "TestUser",
+            // Get password from Mis.Password, otherwise set to AdminAdmin (Hash salt: MySalt)
+            Password = Environment.GetEnvironmentVariable("Mis.Password") ?? "045e9902f2aca62f48117c43dfbd18cb",
+            Email = "TestUser@Users.com",
+            FirstName = "Test",
+            LastName = "User",
+            IsEmailConfirmed = true,
+            IsPhoneNumberConfirmed = true,
+            IsDeleted = false,
+            ActiveCode = "4774d6cf92a744f9b181609003b31e7d",
+            IdentityCode = "045f5b119de54909ab6630eae9a0b532",
+            RegisterTime = DateTime.Now
+        });
+    }
+}
---
+++ Tests/MisProject.DataLayer.Tests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,132 @@
+﻿using AutoFixture;
+using MisProject.Helpers.Converters;
+
+namespace MisProject.DataLayer.Tests.Repositories;
+
+public class UserRepositoryTests
+{
+    private readonly MisDbContext _db = MockData.DbContext;
+    private UserRepository _repository;
+
+    public UserRepositoryTests()
+    {
+        _repository = new UserRepository(_db);
+    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        _db.Database.EnsureDeleted();
+        _db.Database.EnsureCreated();
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        _db.Database.EnsureDeleted();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _db.ChangeTracker.Clear();
+    }
+
+    [Test]
+    public async Task Create_ShouldReturnCreatedUser()
+    {
+        // Arrange
+        var fixture = new UserFixture();
+        var user = fixture.Create<User>();
+
+        // Act
+        var result = await _repository.Create(user);
+
+        // Assert
+        result.Should().NotBeNull().And.BeOfType<User>();
+        result?.UserId.Should().NotBe(0);
+
+        // Cleanup
+        await _repository.Delete(result.UserId);
+    }
+
+    [Test]
+    public async Task GetAll_ShouldReturnAllExistingUsers()
+    {
+        // Act
+        var result = await _repository.GetAll();
+        var users = result.ToList();
+
+        // Assert
+        users.Should().NotBeNull().And.BeOfType<List<User>>();
+        result.Should().HaveCount(UserSectionConfigs.UserSeedList.Count);
+    }
+
+    [Test]
+    public async Task GetById_ShouldReturnExistingUser()
+    {
+        // Arrange
+        var fixture = new UserFixture();
+        var user = fixture.Create<User>();
+        await _repository.Create(user);
+
+        // Act
+        var result = await _repository.GetById(user.UserId);
+
+        // Assert
+        result.Should().NotBeNull().And.BeOfType<User>();
+        result?.UserId.Should().Be(user.UserId);
+
+        // Cleanup
+        await _repository.Delete(user);
+    }
+
+    [Test]
+    public async Task Update_ShouldReturnUpdatedUser()
+    {
+        // Arrange
+        var fixture = new UserFixture();
+        var user = fixture.Create<User>();
+        await _repository.Create(user);
+
+        // Act
+        user.FirstName = "Updated";
+        var result = await _repository.Update(user);
+
+        // Assert
+        result.Should().NotBeNull().And.BeOfType<User>();
+        result?.FirstName.Should().Be("Updated");
+
+        // Cleanup
+        await _repository.Delete(user.UserId);
+    }
+
+    [Test]
+    public async Task DeleteById_ShouldReturnTrue()
+    {
+        // Arrange
+        var fixture = new UserFixture();
+        var user = fixture.Create<User>();
+        await _repository.Create(user);
+
+        // Act
+        var result = await _repository.Delete(user.UserId);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    public async Task Delete_ShouldReturnTrue()
+    {
+        // Arrange
+        var fixture = new UserFixture();
+        var user = fixture.Create<User>();
+        await _repository.Create(user);
+
+        // Act
+        var result = await _repository.Delete(user);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+}
